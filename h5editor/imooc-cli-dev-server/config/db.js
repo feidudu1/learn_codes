7fc28b116547b70a75b35349cee0c9f7f0235e1c
youngdudu <342363079@qq.com>
2024-02-02 16:07:14 +0800

    【前端架构-脚手架】后端项目修改

--- h5editor/imooc-cli-dev-server/config/db.js
+++ h5editor/imooc-cli-dev-server/config/db.js
@@ -1,8 +1,11 @@
 'use strict';
 
 /** MONGODB **/
-const mongodbUrl = 'mongodb://sam:123456@localhost:27017/imooc-cli';
-const mongodbDbName = 'imooc-cli';
+/**
+ * 这里的yafei是 MONGODB 的用户名，4321 是 MONGODB 的密码，都是在 MONGODB 的users incon里添加设置的
+ */
+const mongodbUrl = 'mongodb://yafei:4321@localhost:27017/imooc_cli';
+const mongodbDbName = 'imooc_cli';
 
 module.exports = {
   mongodbUrl,
